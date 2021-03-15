838acaa364e8da12647614465f98b0655af1c5aa
Thomas Madlener <thomas.madlener@desy.de>
2021-03-15 14:42:00 +0100

    Add benchmark instrumentation

--- examples/cpp/delphes2lcio/src/DelphesMain.h
+++ examples/cpp/delphes2lcio/src/DelphesMain.h
@@ -1,9 +1,14 @@
+#ifndef BENCHMARK_INSTRUMENTATION
+#define BENCHMARK_INSTRUMENTATION 0
+#endif
+
 #include "DelphesLCIOConverter_new.h"
 #include "DelphesLCIOOutputConfiguration.h"
 #include "DelphesInputReader.h"
 
 #include "IOIMPL/LCFactory.h"
 
+#include "podio/BenchmarkRecorder.h"
 
 #include "modules/Delphes.h"
 #include "ExRootAnalysis/ExRootConfReader.h"
@@ -22,6 +27,7 @@ void SignalHandler(int /*si*/) {
 
 int runConverter(int argc, char* argv[], DelphesInputReader& inputReader) {
   using namespace delphes_lcio;
+  using namespace podio::benchmark;
 
   // We can't make this a unique_ptr because it interferes with whatever ROOT is
   // doing under the hood to clean up
@@ -44,9 +50,29 @@ int runConverter(int argc, char* argv[], DelphesInputReader& inputReader) {
                                    lcioOutputSettings,
                                    confReader->GetDouble("ParticlePropagator::Bz", 0));
 
-    auto lcWriter = std::unique_ptr<lcio::LCWriter>(lcio::LCFactory::getInstance()->createLCWriter());
-    lcWriter->open(outputFile, lcio::LCIO::WRITE_NEW);
+#if BENCHMARK_INSTRUMENTATION
+    BenchmarkRecorder benchmarkRecorder(outputFile + ".bench.root");
+    auto& setupTree = benchmarkRecorder.addTree("setup_times", {"constructor", "open_file", "close"});
+    auto& eventTree = benchmarkRecorder.addTree("event_times", {"write_event"});
+    auto& loopTree = benchmarkRecorder.addTree("non_io_times",
+                                               {"loop", "read", "process_delphes", "process_convert", "write"});
 
+    const auto constStart = ClockT::now();
+#endif
+    auto lcWriter = std::unique_ptr<lcio::LCWriter>(lcio::LCFactory::getInstance()->createLCWriter());
+#if BENCHMARK_INSTRUMENTATION
+    const auto constEnd = ClockT::now();
+    setupTree.recordTime("constructor", constEnd - constStart);
+    // Cannot figure out how to use run_void_member_timed with LCWriter here,
+    // because open is overloaded
+    const auto openStart = ClockT::now();
+#endif
+    lcWriter->open(outputFile, lcio::LCIO::WRITE_NEW);
+#if BENCHMARK_INSTRUMENTATION
+    const auto openEnd = ClockT::now();
+    setupTree.recordTime("open_file", openEnd - openStart);
+#endif
+   
     // has to happen before InitTask
     TObjArray* allParticleOutputArray = modularDelphes->ExportArray("allParticles");
     TObjArray* stableParticleOutputArray = modularDelphes->ExportArray("stableParticles");
@@ -61,31 +87,58 @@ int runConverter(int argc, char* argv[], DelphesInputReader& inputReader) {
     for (Int_t entry = 0;
          !inputReader.finished() && (maxEvents > 0 ?  entry < maxEvents : true) && !interrupted;
          ++entry) {
-
+#if BENCHMARK_INSTRUMENTATION
+      const auto loopStartTime = podio::benchmark::ClockT::now();
+#endif
       if (!inputReader.readEvent(modularDelphes,
                                  allParticleOutputArray,
                                  stableParticleOutputArray,
                                  partonOutputArray)) {
         break;
       }
-
-      auto* evt = new lcio::LCEventImpl;
-
+#if BENCHMARK_INSTRUMENTATION
+      const auto readEndTime = podio::benchmark::ClockT::now();
+#endif
       modularDelphes->ProcessTask();
+#if BENCHMARK_INSTRUMENTATION
+      const auto delphesProcessEndTime = podio::benchmark::ClockT::now();
+#endif
+      auto evt = std::make_unique<lcio::LCEventImpl>();
+      converter.process(inputReader.converterTree(), evt.get());
+#if BENCHMARK_INSTRUMENTATION
+      const auto convertEndTime = podio::benchmark::ClockT::now();
+#endif
 
-      converter.process(inputReader.converterTree(), evt);
-      lcWriter->writeEvent(evt);
-      delete evt;
-
+#if BENCHMARK_INSTRUMENTATION
+      eventTree.recordTime("write_event",
+                           run_void_member_timed(*lcWriter, &lcio::LCWriter::writeEvent, evt.get()));
+#else
+      lcWriter->writeEvent(evt.get());
+#endif
       modularDelphes->Clear();
       progressBar.Update(eventCounter, eventCounter);
       eventCounter++;
+#if BENCHMARK_INSTRUMENTATION
+      const auto loopEndTime = podio::benchmark::ClockT::now();
+      loopTree.recordTime("loop", loopEndTime - loopStartTime);
+      loopTree.recordTime("read", readEndTime - loopStartTime);
+      loopTree.recordTime("process_delphes", delphesProcessEndTime - readEndTime);
+      loopTree.recordTime("process_convert", convertEndTime - delphesProcessEndTime);
+      loopTree.recordTime("write", loopEndTime - convertEndTime);
+      loopTree.Fill();
+      eventTree.Fill();
+#endif
     }
 
     progressBar.Update(eventCounter, eventCounter, true);
     progressBar.Finish();
     modularDelphes->Finish();
+#if BENCHMARK_INSTRUMENTATION
+    setupTree.recordTime("close", run_void_member_timed(*lcWriter, &lcio::LCWriter::close));
+    setupTree.Fill();
+#else
     lcWriter->close();
+#endif
   } catch (std::runtime_error& e) {
     std::cerr << "** ERROR: " << e.what() << std::endl;
     return 1;
